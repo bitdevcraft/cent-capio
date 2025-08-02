41a0052bd777cf77938ed40f145e331cc57099e9
The Bit Dev <ryanvincent.capio@outlook.com>
2025-08-02 12:06:58 +0000

    Aug 2, 2025, 4:06 PM

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,3 @@
-import { PlateEditor } from "./(frontend)/(protected)/editor/test-editor";
-
 export default async function Page() {
-  return (
-    <>
-      <PlateEditor />
-    </>
-  );
+  return <></>;
 }
